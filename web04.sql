3b050a6688a74e27926cfc5901e7db05d885eb52
kai <cgk0910@gmail.com>
2021-08-10 14:38:10 +0800

    db 檔案

---
+++ web04.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-08-10 14:37:35
+-- 伺服器版本： 10.4.18-MariaDB
+-- PHP 版本： 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `web04`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `acc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pw` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pr` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`, `pr`) VALUES
+(1, 'admin', '1234', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bot`
+--
+
+CREATE TABLE `bot` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `bot` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bot`
+--
+
+INSERT INTO `bot` (`id`, `bot`) VALUES
+(1, '科技大學頁尾版權2021');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `mem`
+--
+
+CREATE TABLE `mem` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `acc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pw` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `addr` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `tel` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `regdate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `mem`
+--
+
+INSERT INTO `mem` (`id`, `acc`, `pw`, `name`, `email`, `addr`, `total`, `tel`, `regdate`) VALUES
+(3, 'mem01', 'mem01', 'mem01', 'mem01@gmail.com', 'fdsaf', 0, 'dsfasdf', '2021-08-10 06:14:52'),
+(4, 'mem02', 'mem02', 'mem02', 'fdssafdasf', 'fdsafd', 0, 'fasdf', '2021-08-10 06:15:07');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `bot`
+--
+ALTER TABLE `bot`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `mem`
+--
+ALTER TABLE `mem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bot`
+--
+ALTER TABLE `bot`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `mem`
+--
+ALTER TABLE `mem`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
