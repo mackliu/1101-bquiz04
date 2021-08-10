26157c0915ae2e67c9d7920a41d823a1f4ce553f
kai <cgk0910@gmail.com>
2021-08-10 14:00:37 +0800

    補強前台登入後的連結顯示及登出功能

--- index.php
+++ index.php
@@ -26,8 +26,23 @@
                 <a href="?do=news">最新消息</a> |
                 <a href="?do=look">購物流程</a> |
                 <a href="?do=buycart">購物車</a> |
-                <a href="?do=login">會員登入</a> |
-                <a href="?do=admin">管理登入</a>
+                <?php
+                if(isset($_SESSION['mem'])){
+                    echo "<a href='?do=logout'>登出</a> |";
+
+                }else{
+                    echo "<a href='?do=login'>會員登入</a> |";
+                }
+                ?>
+                <?php
+                if(isset($_SESSION['admin'])){
+                    echo "<a href='backend.php'>返回管理</a> |";
+
+                }else{
+                    echo "<a href='?do=admin'>管理登入</a> |";
+                }
+                ?>
+                
             </div>
             <marquee>年終特賣會開跑了&nbsp;&nbsp;&nbsp;情人節特惠活動</marquee>
         </div>
