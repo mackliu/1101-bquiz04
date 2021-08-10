cd48cd5dcbef5e3b2b0de6d3ad969b56ff3b9871
kai <cgk0910@gmail.com>
2021-08-10 15:45:20 +0800

    完成後台管理選單權限控制功能

--- backend.php
+++ backend.php
@@ -1,5 +1,15 @@
-
 <?php include_once "base.php";?>
+<?php
+
+if(isset($_SESSION['admin'])){
+    $user=$Admin->find(['acc'=>$_SESSION['admin']]);
+    $mpr=unserialize($user['pr']);
+}else{
+    echo "非法登入";
+    exit();
+}
+
+?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
@@ -26,12 +36,12 @@
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <a href="?do=admin">管理權限設置</a>
-                <a href="?do=th">商品分類與管理</a>
-                <a href="?do=order">訂單管理</a>
-                <a href="?do=mem">會員管理</a>
-                <a href="?do=bot">頁尾版權管理</a>
-                <a href="?do=news">最新消息管理</a>
-                <a href="?do=logout" style="color:#f00;">登出</a>
+                <?=(in_array(1,$mpr))?"<a href='?do=th'>商品分類與管理</a>":'';?>
+                <?=(in_array(2,$mpr))?"<a href='?do=order'>訂單管理</a>":'';?>
+                <?=(in_array(3,$mpr))?"<a href='?do=mem'>會員管理</a>":'';?>
+                <?=(in_array(4,$mpr))?"<a href='?do=bot'>頁尾版權管理</a>":'';?>
+                <?=(in_array(5,$mpr))?"<a href='?do=news'>最新消息管理</a>":'';?>
+                <a href="javascript:location.href='frontend/logout.php'" style="color:#f00;" >登出</a>
             </div>
         </div>
         <div id="right">
