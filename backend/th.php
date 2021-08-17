50b6bbc8556059d5a6981b2fd83e57dc8b4fae2e
kai <cgk0910@gmail.com>
2021-08-17 11:32:53 +0800

    完成編輯商品分類功能

--- backend/th.php
+++ backend/th.php
@@ -34,15 +34,30 @@ foreach ($bigs as $key => $big) {
         echo "<tr class='pp'>";
         echo "<td class='ct'>{$mid['name']}</td>";
         echo "<td class='ct'>";
-        echo "<button onclick='edit(this,{$mid['id']})'>修改</button>";
-        echo "<button onclick=del('type',{$mid['id']})>刪除</button>";
+        echo "  <button onclick='edit(this,{$mid['id']})'>修改</button>";
+        echo "  <button onclick=del('type',{$mid['id']})>刪除</button>";
         echo "</td>";
         echo "</tr>";
     }
 }
 
-
 ?>
 </table>
-
 </div>
+<script>
+function edit(dom,id){
+    let name=$(dom).parent().prev().text()
+    let str=prompt("請輸入你要修改的分類名稱",name)
+    if(str!=null){
+        $.post("api/save_type.php",{'name':str,id},()=>{
+            $(dom).parent().prev().text(str)
+        })
+    }
+}
+
+
+
+</script>
+
+<h1 class="ct">商品管理</h1>
+<div class="ct"><button>新增商品</button></div>
