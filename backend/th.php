c7fabecb0af121c5e85e78f88e91102ec57721ff
kai <cgk0910@gmail.com>
2021-08-17 09:51:41 +0800

    完成新增大分類功能

---
+++ backend/th.php
@@ -0,0 +1,6 @@
+<h1 class="ct">商品分類</h1>
+<div class="ct">
+    <form action="api/add_big.php" method="post">
+        新增大分類<input type="text" name="name" id="typeBig">
+        <input type="submit" value="新增"></form>
+</div>
