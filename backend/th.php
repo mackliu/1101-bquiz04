3e54b5273f077ae6deb4586a956031d26f3be252
kai <cgk0910@gmail.com>
2021-08-17 10:39:38 +0800

    顯示大分類列表

--- backend/th.php
+++ backend/th.php
@@ -1,6 +1,37 @@
 <h1 class="ct">商品分類</h1>
 <div class="ct">
-    <form action="api/add_big.php" method="post">
-        新增大分類<input type="text" name="name" id="typeBig">
+    <form action="api/save_type.php" method="post">
+        新增大分類<input type="text" name="name">
         <input type="submit" value="新增"></form>
+</div>
+<div class="ct">
+    <form action="api/save_type.php" method="post">
+        新增中分類<select name="parent" id="parent">
+            <?php
+                $bigs=$Type->all(['parent'=>0]);
+                foreach ($bigs as $key => $value) {
+                    echo "<option value='{$value['id']}'>{$value['name']}</option>";
+                }
+            ?>            
+        </select>
+        <input type="text" name="name">
+        <input type="submit" value="新增"></form>
+</div>
+<div class="type-list">
+<table class=all>
+<?php
+foreach ($bigs as $key => $big) {
+    echo "<tr class='tt'>";
+    echo "<td>{$big['name']}</td>";
+    echo "<td>";
+    echo "<button onclick='edit(this,{$big['id']})'>修改</button>";
+    echo "<button onclick=del('type',{$big['id']})>刪除</button>";
+    echo "</td>";
+    echo "</tr>";
+}
+
+
+?>
+</table>
+
 </div>
