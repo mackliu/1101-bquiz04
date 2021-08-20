5d8bf01ab47e1027936239ef01ccb4c602ff72b5
kai <cgk0910@gmail.com>
2021-08-20 10:52:28 +0800

    完成商品詳細資料功能

--- frontend/home.php
+++ frontend/home.php
@@ -31,11 +31,13 @@ foreach($rows as $row){
 <table class="all">
     <tr class="pp">
         <td width="40%" height="150px">    
-            <img src="img/<?=$row['img'];?>" style="width:80%;height:80%">
+            <a href='?do=detail&id=<?=$row['id'];?>'><img src="img/<?=$row['img'];?>" style="width:80%;height:80%"></a>
         </td>
         <td>
             <div class="tt ct"><?=$row['name'];?></div>
-            <div>價錢:<?=$row['price'];?></div>
+            <div>價錢:<?=$row['price'];?>
+            <a style="float:right" href='?do=buycart&id=<?=$row['id'];?>&qt=1'><img src="icon/0402.jpg"></a>
+        </div>
             <div>規格:<?=$row['spec'];?></div>
             <div>簡介:<?=mb_substr($row['intro'],0,25);?>...</div>
 
