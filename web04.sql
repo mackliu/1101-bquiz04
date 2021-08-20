ff44625fe640cd23e797796c1a3ebceaf25d5eb3
kai <cgk0910@gmail.com>
2021-08-20 14:42:30 +0800

    更新資料庫

--- web04.sql
+++ web04.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-08-17 16:26:10
+-- 產生時間： 2021-08-20 14:41:51
 -- 伺服器版本： 10.4.18-MariaDB
 -- PHP 版本： 7.3.27
 
@@ -86,10 +86,10 @@ CREATE TABLE `goods` (
 --
 
 INSERT INTO `goods` (`id`, `no`, `name`, `price`, `spec`, `qt`, `img`, `intro`, `big`, `mid`, `sh`) VALUES
-(1, '514051', '大皮帶', 399, '黑色', 10, '0403.jpg', 'dsfasdfsd\r\nfsdfsdfsdf', 1, 13, 1),
+(1, '514051', '大皮帶', 399, '黑色', 10, '0403.jpg', 'dsfasdfsd\r\nfsdfsdfsdf', 1, 13, 0),
 (4, '282064', '手工訂製長夾', 1200, '全牛皮', 2, '0403.jpg', '手工製作長夾卡片層6*2 鈔票層 *2 零錢拉鍊層 *1 \r\n採用愛馬仕相同的雙針縫法,皮件堅固耐用不脫線 \r\n材質:直革鞣(馬鞍皮)牛皮製作  \r\n手工染色 ', 1, 12, 1),
 (5, '926352', '兩用式磁扣腰包', 685, '中型', 18, '0404.jpg', '材質:進口牛皮\r\n顏色:黑色荔枝紋+黑色珠光面皮(黑色縫線)\r\n尺寸:15cm*14cm(高)*6cm(前後)\r\n產地:臺灣', 1, 12, 1),
-(6, '424133', '超薄設計男士長款真皮', 800, 'L號', 61, '0405.jpg', '基本:編織皮革對摺長款零錢包\r\n特色:最潮流最時尚的單品 \r\n顏色:黑色珠光面皮(黑色縫線)\r\n形狀:黑白格編織皮革對摺 ', 1, 12, 1),
+(6, '424133', '超薄設計男士長款真皮', 800, 'L號', 61, '0405.jpg', '基本:編織皮革對摺長款零錢包\r\n特色:最潮流最時尚的單品 \r\n顏色:黑色珠光面皮(黑色縫線)\r\n形狀:黑白格編織皮革對摺 ', 1, 12, 0),
 (7, '816891', '經典牛皮少女帆船鞋', 1000, 'S號', 6, '0406.jpg', '以傳統學院派風格聞名，創始近百年工藝製鞋精神\r\n共用獨家專利氣墊技術，兼具紐約工藝精神，與舒適跑格靈魂', 2, 14, 1),
 (8, '030203', '經典優雅時尚流行涼鞋', 2650, 'LL', 8, '0407.jpg', '優雅流線方型楦頭設計，結合簡潔線條綴飾，\r\n獨特的弧度與曲線美，突顯年輕優雅品味，\r\n是年輕上班族不可或缺的鞋款！\r\n全新美國運回，現貨附鞋盒', 2, 15, 1),
 (9, '040202', '寵愛天然藍寶女戒', 28000, '1克拉', 1, '0408.jpg', '◎典雅設計品味款\r\n◎藍寶為珍貴天然寶石之一，具有保值收藏\r\n◎專人設計製造，以貴重珠寶精緻鑲工製造', 3, 11, 1),
@@ -121,6 +121,32 @@ CREATE TABLE `mem` (
 INSERT INTO `mem` (`id`, `acc`, `pw`, `name`, `email`, `addr`, `total`, `tel`, `regdate`) VALUES
 (3, 'mem01', 'mem01', 'mem01', 'mem01@gmail.com', 'fdsaf', 0, 'dsfasdf', '2021-08-10 06:14:52'),
 (4, 'mem02', 'mem02', 'mem02', 'fdssafdasf', 'fdsafd', 0, 'fasdf', '2021-08-10 06:15:07');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ord`
+--
+
+CREATE TABLE `ord` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `no` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `acc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `addr` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tel` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` int(10) NOT NULL,
+  `goods` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `orddate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `ord`
+--
+
+INSERT INTO `ord` (`id`, `no`, `acc`, `name`, `email`, `addr`, `tel`, `total`, `goods`, `orddate`) VALUES
+(1, '20210820694130', 'mem01', 'mem01', 'mem01@gmail.com', '泰山', '09232112323', 4300, 'a:3:{i:7;s:1:\"1\";i:8;s:1:\"1\";i:11;s:1:\"1\";}', '2021-08-20 06:40:20');
 
 -- --------------------------------------------------------
 
@@ -180,6 +206,12 @@ ALTER TABLE `mem`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `ord`
+--
+ALTER TABLE `ord`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `type`
 --
 ALTER TABLE `type`
@@ -212,6 +244,12 @@ ALTER TABLE `goods`
 --
 ALTER TABLE `mem`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `ord`
+--
+ALTER TABLE `ord`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `type`
